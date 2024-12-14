9dc97ed846b6ecfbffd238f101a6d6f032672035
olevanderheiden <olevanderheiden@hotmail.com>
2024-12-14 18:09:30 +0100

    v0.7 Added toast message for  removing and adding favorites. Curently still generates ref error. This will be fixed in a petch later

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 import { Provider as PaperProvider } from "react-native-paper"; // Provider from react-native-paper
-import { ThemeProvider, useTheme } from "./objects/logic/theme";
+import { ThemeProvider } from "./objects/logic/theme";
 import AppContent from "./objects/logic/navigationHandling";
+import Toast from "react-native-toast-message"; // Import Toast
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
     <ThemeProvider>
       <PaperProvider>
         <AppContent />
+        <Toast ref={(ref) => Toast.setRef(ref)} /> {/* Add Toast component */}
       </PaperProvider>
     </ThemeProvider>
   );
--- objects/logic/favoritesHandling.js
+++ objects/logic/favoritesHandling.js
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { checkFavorites } from "./navigationHandling";
+import Toast from "react-native-toast-message";
 
 export default async function updateFavoriteStatus(landMark, changeLoaded) {
   if (!landMark || !landMark.id) {
@@ -20,12 +21,22 @@ export default async function updateFavoriteStatus(landMark, changeLoaded) {
 
     if (index !== -1) {
       // Remove the landmark from favorites
+      Toast.show({
+        type: "info",
+        text1: "Removed from Favorites",
+        text2: landMark.title.nl,
+      });
       favorites.splice(index, 1);
       console.log("Landmark removed from favorites");
     } else {
       // Add the landmark to favorites
       favorites.push(landMark);
       console.log("Landmark added to favorites");
+      Toast.show({
+        type: "success",
+        text1: "Favorite Added",
+        text2: landMark.title.nl,
+      });
     }
 
     await AsyncStorage.setItem("@favorites", JSON.stringify(favorites));
